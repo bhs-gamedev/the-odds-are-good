ee6272537ce8bbc24c56f2e1415af8e806940da1
Creonalia <52385967+Creonalia@users.noreply.github.com>
2023-03-23 10:26:32 -0700

    setup audio and music

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject settingsMenu;
+    [SerializeField] AudioMixer masterMixer;
     public void OpenGame()
     {
         SceneManager.LoadScene("Game");
@@ -18,4 +22,28 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
 
+    public void OpenSettings()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        masterMixer.SetFloat("MasterVolume", volume);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        masterMixer.SetFloat("MusicVolume", volume);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        masterMixer.SetFloat("SFXVolume", volume);
+    }
 }
